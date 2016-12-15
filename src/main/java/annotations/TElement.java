baf431b38a87c702d0526ae196c1778fb010c7e1
qeqs <vadimlygin@yandex.ru>
2016-12-15 13:06:46 +0300

    annotations

---
+++ src/main/java/annotations/TElement.java
@@ -0,0 +1,16 @@
+package annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.FIELD, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface TElement {
+    String name();
+
+    boolean getter() default false;
+
+    boolean setter() default false;
+}
