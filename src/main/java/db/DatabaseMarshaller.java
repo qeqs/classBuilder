45bdd9fdc8f7636ee5aefd0bbf7851e01c91b540
qeqs <vadimlygin@yandex.ru>
2016-12-15 14:57:01 +0300

    initial commit

--- src/main/java/db/DatabaseMarshaller.java
+++ src/main/java/db/DatabaseMarshaller.java
@@ -1,8 +1,38 @@
 package main.java.db;
 
 import main.java.Marshaller;
+import main.java.annotations.TElement;
+
+import java.lang.reflect.Field;
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DatabaseMarshaller extends Marshaller {
 
+    Connection connection;
+
+    public void marshal(Object object, String name, Object proprety) {
+        connection = (Connection) proprety;
+        Class<?> clazz = object.getClass();
+        String tableName = clazz.getSimpleName();
+        System.out.println(getColoums(clazz));
+    }
+
+    private List<String> getColoums(Class<?> clazz) {
+        ArrayList<String> coloums = new ArrayList<String>();
+        for (Field field : clazz.getDeclaredFields()
+                ) {
+            if (field.isAnnotationPresent(TElement.class)) {
+                String name = field.getAnnotation(TElement.class).name();
+                if (name.equals(""))
+                    coloums.add(field.getName());
+                else
+                    coloums.add("");
+
+            }
+        }
+        return coloums;
+    }
 
 }
