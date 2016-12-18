fcf67ed07f62f5fbed6d92fc28fbfcd3f21f0b92
Анна Семенова <annie@Annas-MacBook-Pro.local>
2016-12-18 23:33:34 +0300

    simple umnarshall(without collections)

--- src/main/java/db/DatabaseUnmarshaller.java
+++ src/main/java/db/DatabaseUnmarshaller.java
@@ -1,27 +1,78 @@
-package main.java.db;
+package db;
 
-import main.java.Unmarshaller;
+import annotations.TElement;
+import annotations.TRootElement;
 
+import java.lang.reflect.Field;
 import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class DatabaseUnmarshaller extends Unmarshaller{
-
-    private static final String url = "jdbc:mysql://localhost:3306/RECIPES_DATABASE?useSSL=false&characterEncoding=UTF-8";
-    private static final String user = "Annie";
-    private static final String password = "Password1";
-
-    public void unmarshall(Object clazz) {
-        try (Connection connection = DriverManager.getConnection(url, user, password)) {
+public class DatabaseUnmarshaller {
 
 
+    public void unmarshall(Object obj, String name, Connection connection) throws SQLException, IllegalAccessException, InstantiationException {
+        if (obj.getClass().isAnnotationPresent(TRootElement.class)) {
+            String tableName = obj.getClass().getAnnotation(TRootElement.class).name();
+            PreparedStatement statement = connection.prepareStatement("select * from " + tableName + " where name=? ");
+            statement.setString(1, name);
+            ResultSet rs = statement.executeQuery();
+            rs.next();
 
+            for (Field field : obj.getClass().getDeclaredFields()) {
+                if (field.isAnnotationPresent(TElement.class)) {
+                    field.setAccessible(true);
 
-        } catch (SQLException ex) {
-            System.out.println("Ошибка подключения к БД");
+                    Class fieldClass = field.getType();
+                    String fieldNameInDB = field.getAnnotation(TElement.class).name();
+
+                    if (fieldClass.isAnnotationPresent(TRootElement.class)) {
+                        //  Object fieldInstance = fieldClass.newInstance();
+                        Object fieldInstance = field.get(obj);
+                        unmarshall(fieldInstance, rs.getInt(fieldNameInDB), connection);
+                        field.set(obj, fieldInstance);
+                    } else {
+                        Object result = rs.getObject(fieldNameInDB);
+                        field.set(obj, result);
+                    }
+                }
+            }
+            statement.close();
+            rs.close();
+
         }
-
-
     }
+
+    private void unmarshall(Object obj, int id, Connection connection) throws SQLException, IllegalAccessException, InstantiationException {
+        if (obj.getClass().isAnnotationPresent(TRootElement.class)) {
+            String tableName = obj.getClass().getAnnotation(TRootElement.class).name();
+            PreparedStatement statement = connection.prepareStatement("select * from " + tableName + " where id=? ");
+            statement.setInt(1, id);
+            ResultSet rs = statement.executeQuery();
+            rs.next();
+
+            for (Field field : obj.getClass().getDeclaredFields()) {
+                if (field.isAnnotationPresent(TElement.class)) {
+                    field.setAccessible(true);
+
+                    Class fieldClass = field.getType();
+                    String fieldNameInDB = field.getAnnotation(TElement.class).name();
+
+                    if (fieldClass.isAnnotationPresent(TRootElement.class)) {
+                        Object fieldInstance = field.get(obj);
+                        unmarshall(fieldInstance, rs.getInt(fieldNameInDB), connection);
+                        field.set(obj, fieldInstance);
+                    } else {
+                        Object result = rs.getObject(fieldNameInDB);
+                        field.set(obj, result);
+                    }
+                }
+            }
+            statement.close();
+            rs.close();
+
+        }
+    }
+
 }
