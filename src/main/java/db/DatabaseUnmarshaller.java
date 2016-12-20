8d54a994f3c581ee1338a92ad969edc86b9b304b
Анна Семенова <annie@Annas-MacBook-Pro.local>
2016-12-20 14:04:38 +0300

    collections

--- src/main/java/db/DatabaseUnmarshaller.java
+++ src/main/java/db/DatabaseUnmarshaller.java
@@ -8,6 +8,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Collection;
+import java.util.Iterator;
 
 public class DatabaseUnmarshaller {
 
@@ -27,14 +29,20 @@ public class DatabaseUnmarshaller {
                     Class fieldClass = field.getType();
                     String fieldNameInDB = field.getAnnotation(TElement.class).name();
 
-                    if (fieldClass.isAnnotationPresent(TRootElement.class)) {
+                    if (Collection.class.isAssignableFrom(fieldClass)) {
+                        Collection collection = unmarshallCollection(rs.getInt(fieldNameInDB), field.get(obj), connection);
+                        field.set(obj, collection);
+                    } else if (fieldClass.isAnnotationPresent(TRootElement.class)) {
                         Object fieldInstance = field.get(obj);
                         unmarshall(fieldInstance, rs.getInt(fieldNameInDB), connection);
                         field.set(obj, fieldInstance);
+                    } else if (fieldClass.isArray()) {
+
                     } else {
                         Object result = rs.getObject(fieldNameInDB);
                         field.set(obj, result);
                     }
+
                 }
             }
             statement.close();
@@ -43,7 +51,7 @@ public class DatabaseUnmarshaller {
         }
     }
 
-    private void unmarshall(Object obj, int id, Connection connection) throws SQLException, IllegalAccessException, InstantiationException {
+    private void unmarshall(Object obj, int id, Connection connection) throws SQLException, IllegalAccessException {
         if (obj.getClass().isAnnotationPresent(TRootElement.class)) {
             String tableName = obj.getClass().getAnnotation(TRootElement.class).name();
             PreparedStatement statement = connection.prepareStatement("select * from " + tableName + " where id=? ");
@@ -74,8 +82,30 @@ public class DatabaseUnmarshaller {
         }
     }
 
-    private void unmarshallCollection(){
+    private Collection unmarshallCollection(int id, Object collectionObject, Connection connection) throws SQLException, IllegalAccessException {
 
+        //получаем сведения о коллекции
+        PreparedStatement statement = connection.prepareStatement("select * from  Collection where id=? ");
+        statement.setInt(1, id);
+        ResultSet rs = statement.executeQuery();
+        rs.next();
+        String tableName = rs.getString("value");
+
+        //получаем содержимое коллекции
+        statement = connection.prepareStatement("select * from " + tableName + " where collection_id=? ");
+        statement.setInt(1, id);
+        ResultSet rs1 = statement.executeQuery();
+
+        Collection collection = (Collection) collectionObject;
+        Iterator<Connection> iterator = collection.iterator();
+
+        while (rs1.next()) {
+            Object current = iterator.next();
+            int currentId = rs1.getInt("id");
+            unmarshall(current, currentId, connection);
+
+        }
+        return collection;
     }
 
 }
